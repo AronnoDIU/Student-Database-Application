2eebdb6a114193250fb976652cb6f4888fdc84dd
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-11-11 12:47:51 +0600

    Student Database Application

--- src/Student.java
+++ src/Student.java
@@ -1,78 +1,94 @@
 import java.util.Scanner;
 
 public class Student {
-    public Scanner s = new Scanner(System.in);
+    public Scanner userInput = new Scanner(System.in);
 
-    // Setting up the variables
-    // Defined as 'private' so that these cannot be accessed directly
+    // Setting up the variables Defined as 'private' so that these cannot be accessed directly
     private static int noOfStudents = 0;
-    private String name;
-    private int year;
-    private String id;
+    private final String studentName;
+    private final int enrollYear;
+    private final String studentID;
     private int balance;
-    private String[] coursesArr = new String[5];
+    private final String[] coursesList = new String[5]; // To store the courses
 
     // Constructor to receive the student's name and year
     public Student() {
-        noOfStudents++;
+        noOfStudents++; // Initializing and Incrementing the number of students
 
-        System.out.print("Enter full name: ");
-        this.name = s.nextLine();
+        System.out.print("Enter the full name of the student: ");
+        this.studentName = userInput.nextLine();
+
         System.out.println("1 - Freshman\n2 - Sophomore\n3 - Junior\n4 - Senior");
+
         System.out.print("Enter year: ");
-        this.year = s.nextInt();
+        this.enrollYear = userInput.nextInt();
 
         // Setting the student's ID
-        this.id = generate_ID();
+        this.studentID = generateID();
     }
 
     // Generating the 5-digit unique ID
-    private String generate_ID() {
-        String id = this.year + "";
-        int nod = (int) Math.floor(Math.log10(noOfStudents) + 1);
-        for (int i = 0; i < 4 - nod; i++) {
-            id += "0";
-        }
-        id += noOfStudents;
-        return id;
+    private String generateID() {
+        // Converting the year to string
+        StringBuilder studentID = new StringBuilder(this.enrollYear + "");
+
+        // Getting the number of digits in noOfStudents variable
+        // and adding 1 to it to get the number of digits in the ID variable.
+        int numberOfDigits = (int) Math.floor(Math.log10(noOfStudents) + 1);
+
+        // Adding leading zeros to the ID variable to make it 5 digits long if necessary.
+        studentID.append("0".repeat(Math.max(0, 4 - numberOfDigits)));
+        studentID.append(noOfStudents); // Appending the number of students
+        return studentID.toString(); // Returning the ID
     }
 
     // Enroll in courses
-    public void courses() {
-        int flag = -1;
+    public void coursesENROLL() {
+        // To store the index of the courses in the coursesList array if it is not null.
+        int flag = -1; // To count the number of courses
 
-        System.out.println("ENTER THE COURSES YOU WISH TO ENTER\n" + "1.History\n2.Maths\n3.English\n4.Chemistry\n"
-                + "5.Comp Science");
+        System.out.println("""
+
+                ENTER THE COURSES YOU WISH TO ENTER
+                1.History
+                2.Maths
+                3.English
+                4.Chemistry
+                5.Comp Science""");
 
         while (true) {
-            System.out.println("ENTER CODE OF COURSE TO ENROLL // -1 TO EXIT: ");
-            int choice = s.nextInt();
+            System.out.println("ENTER CODE OF COURSE TO ENROLL // ENTER 9 TO EXIT: ");
+            int choice = userInput.nextInt();
 
-            if (choice == -1) {
+            if (choice == 9) {
                 break;
             } else {
+
+                // If the choice is valid
                 if (choice > 0 && choice < 6) {
-                    flag++;
-                    this.balance += 600;
+
+                    flag++; // Incrementing the index
+                    this.balance += 600; // Increasing the balance
+
                     switch (choice) {
                         case 1:
-                            this.coursesArr[flag] = "History";
+                            this.coursesList[flag] = "History";
                             break;
                         case 2:
-                            this.coursesArr[flag] = "Maths";
+                            this.coursesList[flag] = "Maths";
                             break;
                         case 3:
-                            this.coursesArr[flag] = "English";
+                            this.coursesList[flag] = "English";
                             break;
                         case 4:
-                            this.coursesArr[flag] = "Chemistry";
+                            this.coursesList[flag] = "Chemistry";
                             break;
                         case 5:
-                            this.coursesArr[flag] = "Comp Science";
+                            this.coursesList[flag] = "Computer Science";
                             break;
                     }
                 } else {
-                    System.out.println("ENTER A VALID CHOICE!");
+                    System.out.println("INVALID CHOICE! PLEASE ENTER AGAIN!");
                 }
             }
         }
@@ -80,33 +96,37 @@ public class Student {
 
     // View balance
     private void viewBalance() {
-        System.out.println("Your Balance is: $" + this.balance);
+        System.out.println("Your Current Balance is : $" + this.balance);
     }
 
     // Pay Tuition
     public void payTuition() {
         this.viewBalance();
-        System.out.print("Enter the amount you want to pay: $");
-        int payment = s.nextInt();
-        this.balance = this.balance - payment;
-        System.out.println("Thanks for the payment!");
-        this.viewBalance();
+        System.out.print("Enter the amount of money that you want to pay: $");
+        int makePayment = userInput.nextInt();
+        this.balance = this.balance - makePayment;
+        System.out.println("Thanks for the payment of $" + makePayment + " !");
+        this.viewBalance(); // Displaying the balance
     }
 
     // Returning the student's courses
     private String showCourses() {
-        String str = "";
-        for (String course : this.coursesArr) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        // Iterating through the coursesList array and appending the courses to the StringBuilder.
+        for (String course : this.coursesList) {
             if (course != null)
-                str += course + ", ";
+                stringBuilder.append(course).append(", ");
         }
-        return str;
+        return stringBuilder.toString(); // Returning the courses
     }
 
     // Displaying the student's information
-    public void showInfo() {
-        System.out.println("Name: " + this.name + "\nID: " + this.id + "\nCourses Enrolled: " + this.showCourses());
-        this.viewBalance();
+    public void showStudentInfo() {
+        System.out.println("Name: " + this.studentName + "\nID: " + this.studentID
+                + "\nEnrolled Year: " + this.enrollYear + "\nCourses Enrolled: " + this.showCourses());
+
+        this.viewBalance(); // Displaying the balance
 
     }
 }
--- src/StudentDatabase.java
+++ src/StudentDatabase.java
@@ -2,41 +2,66 @@ import java.util.Scanner;
 
 public class StudentDatabase {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
+        Scanner userInput = new Scanner(System.in);
 
-        // Ask how many new students we want to add
+        // Ask how many new students we want to add to the database
         System.out.print("NO OF STUDENTS? ");
-        int n = s.nextInt();
+        int NumberS = userInput.nextInt(); // Get the total Number of students
 
-        // Create an array of Student objects of size 'n'
-        Student[] students = new Student[n];
-        for (int i = 0; i < n; i++) {
+        // Create an array of Student objects of size 'NumberS'
+        Student[] students = new Student[NumberS];
+        for (int i = 0; i < NumberS; i++) {
             students[i] = new Student();
-            int choice = -1;
+            int choice; // To store the user's choice
 
             do {
-                System.out.println("\n**************************\nENTER YOUR CHOICE\n1.Show Info\n2.Enrollment"
-                        + "\n3.Pay Tuition\n4.Exit");
-                choice = s.nextInt();
+                System.out.println("""
+
+                        **************************
+                        ENTER YOUR CHOICE
+                        1.Show Info
+                        2.Enrollment
+                        3.Pay Tuition
+                        4.Exit""");
+
+                choice = userInput.nextInt(); // Get the user's choice
+
                 switch (choice) {
                     case 1:
-                        students[i].showInfo();
+                        students[i].showStudentInfo(); // Display the student's information
                         break;
                     case 2:
-                        students[i].courses();
+                        students[i].coursesENROLL(); // Enroll in courses
                         break;
                     case 3:
-                        students[i].payTuition();
+                        students[i].payTuition(); // Pay tuition
                         break;
                     case 4:
-                        System.out.println("\nTHANKS!!!");
+                        System.out.println("\nTHANKS FOR USING THE APPLICATION!");
                         break;
                     default:
-                        System.out.println("INVALID CHOICE! ENTER AGAIN!");
+                        System.out.println("INVALID CHOICE! PLEASE ENTER AGAIN!");
                 }
-
             } while (choice != 4);
-
         }
     }
 }
+
+/* Expected Output:
+
+Application should do the following:
+
+    Ask the user how many new students will be added to the database
+    The user should be prompted to enter the name and year for each student
+    The student should have a 5-digit unique ID, with the first number being their grade level
+    A student can enroll in the following courses:
+        History
+         Mathematics,
+        English
+        Chemistry
+        Computer Science
+    Each course costs $600 to enroll
+    The students should be able to view their balance and pay the tuition
+    To see the status of the student, we should see their name, ID, courses enrolled, and balance
+
+*/
